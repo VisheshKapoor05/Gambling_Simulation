f043aceb5f703f678cca731f16a143f98ac440c3
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-20 10:15:37 +0530

    UC2: added loop to play till defined conditions

--- Gambler_Simulator.java
+++ Gambler_Simulator.java
@@ -4,25 +4,31 @@ public class Gambler_Simulator {
 	
 	public static void main(String[] args) {
 		
-		int dailyStake = 100;
-		final int BET = 1; // or Bet_Per_Game = 1;
+		int dailyStake = 100;        //Daily Stake variable
+		final int BET = 1;           //defining Constant variable for bet per play
+		// or Bet_Per_Game = 1;
 		
 		int LOSE = 0;
 		
-		//getting 0 or 1 for play
-		int play = (int) (Math.random()*10)%2;
-		
-		//Checking for win and lose
-		if(play == LOSE)
-		{	
-			dailyStake -=  BET ;
-		}
-		else                     // play = 1 which means a win
+		//checking for limits of dailyStake
+		while(dailyStake>50 && dailyStake<150)
 		{
-			dailyStake += BET ;
+			//getting random value for play (0 or 1)
+			int play = (int) (Math.random()*10)%2;
+			
+			//Checking for win and lose
+			if(play == LOSE)
+			{	
+				dailyStake -=  BET ;
+			}
+			else                     // play = 1 which means a win
+			{
+				dailyStake += BET ;
+			}
+				
 		}
-		
 		System.out.println("New Balance: " +dailyStake);
+		
 	}
 
 }
