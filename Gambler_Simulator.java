203a83a4a50f8baee7a0d5b8ba30bd5e07e9863e
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-20 09:29:15 +0530

    UC2: added condition statement to check for win or lose

--- Gambler_Simulator.java
+++ Gambler_Simulator.java
@@ -4,9 +4,25 @@ public class Gambler_Simulator {
 	
 	public static void main(String[] args) {
 		
-		int stake = 100;
-		final int BET = 1;
+		int dailyStake = 100;
+		final int BET = 1; // or Bet_Per_Game = 1;
 		
+		int LOSE = 0;
+		
+		//getting 0 or 1 for play
+		int play = (int) (Math.random()*10)%2;
+		
+		//Checking for win and lose
+		if(play == LOSE)
+		{	
+			dailyStake -=  BET ;
+		}
+		else                     // play = 1 which means a win
+		{
+			dailyStake += BET ;
+		}
+		
+		System.out.println("New Balance: " +dailyStake);
 	}
 
 }
